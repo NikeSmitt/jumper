fa25c240db00750b229cd9d57598f4cb4e41a32a
gr <grim322@bk.ru>
2023-03-05 22:04:57 +0300

    Попытка исправить EMAILBACKEND

--- servise/settings/production.py
+++ servise/settings/production.py
@@ -11,3 +11,5 @@ EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS')
 EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
